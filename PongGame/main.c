fc35006486fe641d239df886c4f64a26efb1316a
Bhaskar Abhiraman <bhaskar.abhiraman@gmail.com>
2019-02-17 20:30:23 -0500

    Commit pls

---
+++ PongGame/main.c
@@ -0,0 +1,56 @@
+#define F_CPU 16000000UL
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <util/delay.h>
+#include "lcd.h"
+
+#define FREQ 16000000
+#define BAUD 9600
+#define HIGH 1
+#define LOW 0
+#define BUFFER 1024
+#define BLACK 0x000001
+
+char displayChar = 0;
+
+
+int main(void)
+{
+	//setting up the gpio for backlight
+	DDRD |= 0x80;
+	PORTD &= ~0x80;
+	PORTD |= 0x00;
+	
+	DDRB |= 0x05;
+	PORTB &= ~0x05;
+	PORTB |= 0x00;
+	
+	//lcd initialisation
+	lcd_init();
+	lcd_command(CMD_DISPLAY_ON);
+	lcd_set_brightness(0x18);
+	write_buffer(buff);
+	_delay_ms(1000); //10k
+	clear_buffer(buff);
+	
+	char String[] = "Hello, world!";
+	while (1)
+	{
+		//drawchar(buff,64,0,displayChar);
+		//setpixel(buff, 64, 32, 1);
+		int size = sizeof(String);
+		drawline(buff,1,1,64,32,1);
+		write_buffer(buff);
+		_delay_ms(1000); //5k
+		//clearpixel(buff, 64, 32);
+		//write_buffer(buff);
+		//_delay_ms(1000);
+		//displayChar++;
+	}
+}
+
